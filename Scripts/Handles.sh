cccccc9373ee4ca40932c4ee8d938763a291c8a1
dhlsam <dhlsam1987@gmail.com>
2025-11-20 22:48:31 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -2,13 +2,6 @@
 
 PKG_PATH="$GITHUB_WORKSPACE/wrt/package/"
 
-# 更新 Golang 为最新版
-cd "$pkgPath"
-rm -rf "$WRT_MainPath/feeds/packages/lang/golang"
-git clone https://github.com/sbwml/packages_lang_golang -b 25.x "$WRT_MainPath/feeds/packages/lang/golang"
-echo 'Updated: golang'
-echo ''
-
 #预置HomeProxy数据
 if [ -d *"homeproxy"* ]; then
 	echo " "
@@ -29,38 +22,6 @@ if [ -d *"homeproxy"* ]; then
 	cd .. && rm -rf ./$HP_RULE/
 
 	cd $PKG_PATH && echo "homeproxy date has been updated!"
-fi
-
-#预置OpenClash内核和数据
-if [ -d *"openclash"* ]; then
-        echo "预置OpenClash内核和数据!"
-	CORE_VER="https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version"
-	CORE_TYPE=$(echo $WRT_TARGET | grep -Eiq "64|86" && echo "amd64" || echo "arm64")
-	CORE_TUN_VER=$(curl -sL $CORE_VER | sed -n "2{s/\r$//;p;q}")
-
-	CORE_DEV="https://github.com/vernesong/OpenClash/raw/core/dev/dev/clash-linux-$CORE_TYPE.tar.gz"
-	CORE_MATE="https://github.com/vernesong/OpenClash/raw/core/dev/meta/clash-linux-$CORE_TYPE.tar.gz"
-	CORE_TUN="https://github.com/vernesong/OpenClash/raw/core/dev/premium/clash-linux-$CORE_TYPE-$CORE_TUN_VER.gz"
-
-	GEO_MMDB="https://github.com/alecthw/mmdb_china_ip_list/raw/release/lite/Country.mmdb"
-	GEO_SITE="https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geosite.dat"
-	GEO_IP="https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geoip.dat"
-
-	cd ./luci-app-openclash/root/etc/openclash/
-
-	curl -sL -o Country.mmdb $GEO_MMDB && echo "Country.mmdb done!"
-	curl -sL -o GeoSite.dat $GEO_SITE && echo "GeoSite.dat done!"
-	#curl -sL -o GeoIP.dat $GEO_IP && echo "GeoIP.dat done!"
-
-	mkdir ./core/ && cd ./core/
-
-	curl -sL -o meta.tar.gz $CORE_MATE && tar -zxf meta.tar.gz && mv -f clash clash_meta && echo "meta done!"
-	#curl -sL -o tun.gz $CORE_TUN && gzip -d tun.gz && mv -f tun clash_tun && echo "tun done!"
-	#curl -sL -o dev.tar.gz $CORE_DEV && tar -zxf dev.tar.gz && echo "dev done!"
-
-	chmod +x ./* && rm -rf ./*.gz
-
-	cd $PKG_PATCH && echo "openclash date has been updated!"
 fi
 
 #修改argon主题字体和颜色
